4f71894a94c2240e937a38a3b192dd45cb5e6e26
paledroid <82098580+paledroid@users.noreply.github.com>
2021-04-15 20:56:27 +0100

    Update design.js

--- design.js
+++ design.js
@@ -1,36 +1,30 @@
 // Select color input
 // Select size input
 
-// This code gets the input height and width from the user input in the HTML
+// This code adds user inputs to variables
 
-  var grid = document.getElementById("pixelCanvas");
-  var height = document.getElementById("inputHeight");
-  var width = document.getElementById("inputWidth");
-  var color = document.getElementById("colorPicker");
-  var form = document.getElementById("sizePicker");
+  var g = document.getElementById("pixelCanvas");
+  var h = document.getElementById("inputHeight");
+  var w = document.getElementById("inputWidth");
+  var c = document.getElementById("colorPicker");
+  var f = document.getElementById("sizePicker");
 
-// This code calls makeGrid whhen size is submitted by the user
+// This code calls makeGrid when grid size is submitted by the user
 
-form.addEventListener('submit', makeGrid(height, width));
+f.addEventListener('submit', makeGrid(g,h,w));
 
 // This code defines the function makeGrid
 
-function makeGrid(height, width) {
+function makeGrid(g,h,w) {
 
-// This code creates the grid...
-// firstly by looping over the height input to create rows...
-// and then by appending each row to the table pixelCanvas
+// This code creates a nested loop and adds rows and cells to the table g
 
-  for (var h = 1; h < inputHeight; h++) {
-    var row = document.createElement("tr");
-    document.grid.appendChild(row);
-
-// This code them creates the cells in each row...
-// and appends it to the table too
-
-    for (var w = 1; w < inputWidth; w++) {
-      var cell = document.createElement("td");
-      document.grid.appendChild(cell);
+  for (var row = 1; row < h; row++) {
+    var x = g.insertRow(row);
+    document.g.appendChild(x);
+    for (var cell = 1; cell < w; cell++) {
+      var y = g.insertCell(cell);
+      document.g.appendChild(y);
     }
   }
-}
+};
