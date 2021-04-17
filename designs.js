7a93da62313540454d6612007d0d1638e744ca18
paledroid <82098580+paledroid@users.noreply.github.com>
2021-04-17 15:32:18 +0100

    Version4
    Reverted to advice from tutor and added code to colour cells

--- designs.js
+++ designs.js
@@ -1,32 +1,40 @@
-// Select color input
-// Select size input
+// This code adds user inputs to table and colour variables
+// The variables are in global scope
 
-// This code adds user inputs to variables
+var grid = document.getElementById("pixelCanvas"); // assigns pixelCanvas to variable grid
+var rows = document.getElementById("inputHeight"); // assigns inputHeight to variable rows
+var columns = document.getElementById("inputWidth"); // assigns inputWidth to variable columns
+var colour = document.getElementById("colorPicker"); // assigns colorPicker to variable colour
+var form = document.getElementById("sizePicker"); // assigns sizePicker to variable form
 
-  var grid = document.getElementById("pixelCanvas");
-  var height = document.getElementById("inputHeight");
-  var width = document.getElementById("inputWidth");
-  var colour = document.getElementById("colorPicker");
-  var form = document.getElementById("sizePicker");
+// This code defines the function makeGrid
+// It creates a nested loop and adds rows and cells to the table grid
+
+function makeGrid(rows, columns) {
+    for (a = 0; a < rows.value; a++) {
+      let row = grid.insertRow(0);
+      for (b = 0; b < columns.value; b++) {
+        let cell = row.insertCell(0);
+      }
+    }
+};
 
 // This code calls makeGrid when grid size is submitted by the user
 
-form.addEventListener('submit', makeGrid(grid, height, width);
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  makeGrid(rows, columns)
+});
 
-// This code defines the function makeGrid
-// It creates a nested loop...
-// And adds rows and cells to the table grid
+// This code removes the grid when the user clicks on 'reset'
+// Note that additional html was added to the html file to add a 'reset' button
 
-function makeGrid(grid, height, width) {
-  if height + width = 2 {
-    event.preventDefault();
-  }
-  else {
-    for (var a = 0; a < height.value; a++) {
-      var row = grid.insertRow(0);
-      for (var b = 0; b < width.value; b++) {
-        var cell = row.insertCell(0);
-    }
-      }
-  }
+form.addEventListener('reset', function () {
+  grid.innerHTML=""
+});
+
+// This code enables the user to colour individual cells
+
+grid.addEventListener('click', function (event) {
+  event.target.style.backgroundColor = colour.value;
 });
