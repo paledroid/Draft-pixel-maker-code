2fce548c54c6b7c454360e708412fa371fa24cdc
paledroid <82098580+paledroid@users.noreply.github.com>
2021-04-17 15:53:51 +0100

    Version 5
    Code refactored to improve structure and for ease of reading

--- designs.js
+++ designs.js
@@ -1,16 +1,22 @@
-// This code adds user inputs to table and colour variables
-// The variables are in global scope
+// Notes on scope
+  // The variables are in global scope as the code is simple
+  // The grid variables in makeGrid are defined only in local scope
 
+// Note on code structure
+  // Functions are declared at the top of the script for good practice
+  // addEventListener code has been refactored into a function
+
+// This code adds user inputs to table and colour variables
 var grid = document.getElementById("pixelCanvas"); // assigns pixelCanvas to variable grid
 var rows = document.getElementById("inputHeight"); // assigns inputHeight to variable rows
 var columns = document.getElementById("inputWidth"); // assigns inputWidth to variable columns
 var colour = document.getElementById("colorPicker"); // assigns colorPicker to variable colour
 var form = document.getElementById("sizePicker"); // assigns sizePicker to variable form
 
-// This code defines the function makeGrid
-// It creates a nested loop and adds rows and cells to the table grid
+// Defines the function makeGrid
 
 function makeGrid(rows, columns) {
+    // Creates a nested loop and adds rows and cells to the table grid
     for (a = 0; a < rows.value; a++) {
       let row = grid.insertRow(0);
       for (b = 0; b < columns.value; b++) {
@@ -19,22 +25,27 @@ function makeGrid(rows, columns) {
     }
 };
 
-// This code calls makeGrid when grid size is submitted by the user
+// Defines the function pixelMaker to refactor individual lines of code
 
-form.addEventListener('submit', function (event) {
-  event.preventDefault();
-  makeGrid(rows, columns)
-});
+function pixelMaker () {
 
-// This code removes the grid when the user clicks on 'reset'
-// Note that additional html was added to the html file to add a 'reset' button
+    // calls makeGrid when grid size is submitted by the user
+    form.addEventListener('submit', function (event) {
+      event.preventDefault();
+      makeGrid(rows, columns)
+    });
 
-form.addEventListener('reset', function () {
-  grid.innerHTML=""
-});
+    // enables the user to colour individual cells
+    grid.addEventListener('click', function (event) {
+      event.target.style.backgroundColor = colour.value;
+    });
 
-// This code enables the user to colour individual cells
+    // Removes the grid when the user clicks on 'reset'
+    // Note that 'reset' button was added to the html file
+    form.addEventListener('reset', function () {
+      grid.innerHTML=""
+    });
+};
 
-grid.addEventListener('click', function (event) {
-  event.target.style.backgroundColor = colour.value;
-});
+// calls the pixelMaker function
+pixelMaker();
